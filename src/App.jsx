84720db1f7312bc5ea0bcd868955d7ed90b0ccbd
Carlitos73490 <71330622+Carlitos73490@users.noreply.github.com>
2022-05-03 12:32:04 +0200

    Test Style + Npm package import

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,13 @@ import { Link,Outlet } from "react-router-dom";
  function App() {
   return (
       <div>
-          <h1>Kindred</h1>
+          <div className="d-flex align-items-center w-100 App-Header">
+              <span className="icon">
+                <i className="bi bi-list"></i>
+            </span>
+              <h1 className="App-Logo">Kindred</h1>
+          </div>
+
           <nav>
               <Link to="/dashboard">Dashboard</Link>
               <Link to="/campaign">Campaign</Link>
